d9fec2a671b3280ee21c0be71645e0de9e2ec10e
Jailine <jay.santana964@gmail.com>
2024-05-06 21:29:22 -0300

    Iniciando o projeto - Criação da classe Produto

---
+++ Projeto-Servico-web/src/main/java/com/example/demo/entities/Produto.java
@@ -0,0 +1,66 @@
+package com.example.demo.entities;
+
+import java.util.Objects;
+
+public class Produto {
+	
+	private Long Id;
+	private String nome;
+	private Double preco;
+	private String descricao;
+	
+	public Produto() {
+
+	}
+	
+	public Produto(Long id, String nome, Double preco, String descricao) {
+		Id = id;
+		this.nome = nome;
+		this.preco = preco;
+		this.descricao = descricao;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Id, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(Id, other.Id) && Objects.equals(nome, other.nome);
+	}
+	
+
+}
